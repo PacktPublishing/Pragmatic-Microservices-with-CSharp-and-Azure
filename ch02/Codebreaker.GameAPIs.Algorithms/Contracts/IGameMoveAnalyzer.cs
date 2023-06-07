a5aad48b3140beac8b9c8d705e1e76e0e405cf0c
Christian Nagel <christian@christiannagel.com>
2023-06-07 21:51:20 +0200

    documentation added to igamemoveanalyzer

--- ch02/Codebreaker.GameAPIs.Algorithms/Contracts/IGameMoveAnalyzer.cs
+++ ch02/Codebreaker.GameAPIs.Algorithms/Contracts/IGameMoveAnalyzer.cs
@@ -2,6 +2,13 @@
 
 public interface IGameMoveAnalyzer
 {
+    /// <summary>
+    /// Applies a move to the game after the move is analzyed. The result of the move analysis is returned.
+    /// </summary>
+    /// <returns>The result of the move that is parsable</returns>
+    /// <exception cref="ArgumentException" HResult="4200">Thrown when the number of guesses does not match the number of holes in the game</exception>
+    /// <exception cref="ArgumentException" HRESULT="4300">Thrown when the move number is not the next move number in the game</exception>
+    /// <exception cref="ArgumentException" HRESULT="4400..4404">Thrown when the guess contains an invalid value</exception></exception>
     string ApplyMove();
 }
 
