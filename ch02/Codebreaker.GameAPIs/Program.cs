50ec0e4aa39a1ecac917d7f8a12ecbc7ac9b2618
Christian Nagel <christian@christiannagel.com>
2023-06-07 08:54:45 +0200

    json configuration

--- ch02/Codebreaker.GameAPIs/Program.cs
+++ ch02/Codebreaker.GameAPIs/Program.cs
@@ -4,8 +4,6 @@ using System.Text.Json.Serialization;
 using Codebreaker.GameAPIs.Data;
 using Codebreaker.GameAPIs.Data.InMemory;
 
-using Microsoft.AspNetCore.Http.Json;
-
 [assembly: InternalsVisibleTo("Codbreaker.APIs.Tests")]
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,20 +18,13 @@ builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
-
-//builder.Services.Configure<JsonOptions>(options =>
-//{
-//    options.SerializerOptions.AddContext<GamesJsonSerializerContext>();
-//});
-
 // Swagger/EndpointDocumentation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IGamesRepository, GamesMemoryRepository>();
-
 // Application Services
 
+builder.Services.AddSingleton<IGamesRepository, GamesMemoryRepository>();
 builder.Services.AddScoped<IGamesService, GamesService>();
 
 var app = builder.Build();
