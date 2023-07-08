dcac4e55e19afb6a5574e3359d43bff9413be48c
Christian Nagel <christian@christiannagel.com>
2023-07-08 13:37:08 +0200

    cleanup cosmos context

--- ch03/Codebreaker.Data.Cosmos/GamesCosmosContext.cs
+++ ch03/Codebreaker.Data.Cosmos/GamesCosmosContext.cs
@@ -7,12 +7,6 @@ namespace Codebreaker.Data.Cosmos;
 
 public class GamesCosmosContext : DbContext, IGamesRepository
 {
-    public const string PartitionKey = nameof(PartitionKey);
-    public const string GameId = nameof(GameId);
-    public const string MoveId = nameof(MoveId);
-    public const string Id = nameof(Id);
-    public const string ETag = nameof(ETag);
-
     private FieldValueValueConverter _fieldValueConverter = new();
     private FieldValueComparer _fieldValueComparer = new();
 
@@ -27,50 +21,20 @@ public class GamesCosmosContext : DbContext, IGamesRepository
         modelBuilder.Entity<Game>().HasPartitionKey(g => g.GameId);
         var gameModel = modelBuilder.Entity<Game>();
 
-        //gameModel.Ignore(g => g.GameId);
-        //gameModel.Ignore(g => g.Moves);
-        // gameModel.HasPartitionKey(PartitionKey);
-        // gameModel.Property<string>(PartitionKey);
-        // gameModel.Property<string>(ETag).IsETagConcurrency();
-
         gameModel.Property(g => g.FieldValues)
             .HasConversion(_fieldValueConverter, _fieldValueComparer);
-
-        // modelBuilder.Owned<Move>();
-
-         //gameModel
-         //   .OwnsMany(g => g.Moves, moveBuilder =>
-         //   {
-         //       moveBuilder.ToJsonProperty("Moves");
-         //       moveBuilder.Property<string>(PartitionKey);
-         //       moveBuilder.Property<Guid>(GameId);
-         //       //moveBuilder.Property<Guid>(MoveId).ValueGeneratedOnAdd();
-         //       //moveBuilder.WithOwner().HasForeignKey(GameId, PartitionKey);
-         //       //moveBuilder.HasKey(MoveId);
-         //   });
     }
-
-    public static string ComputePartitionKey(Game game) => game.GameId.ToString();
-
-    public void SetPartitionKey(Game game) =>
-        Entry(game).Property(PartitionKey).CurrentValue =
-            ComputePartitionKey(game);
-
-    //public void SetMoveId(Move move) =>
-    //    Entry(move).Property<Guid>(MoveId).CurrentValue = Guid.NewGuid();
 
     public DbSet<Game> Games => Set<Game>();
 
     public async Task AddGameAsync(Game game, CancellationToken cancellationToken = default)
     {
-        // SetPartitionKey(game);
         Games.Add(game);
         await SaveChangesAsync(cancellationToken);
     }
 
     public async Task AddMoveAsync(Game game, Move move, CancellationToken cancellationToken = default)
     {
-        // SetMoveId(move);
         Games.Update(game);
         await SaveChangesAsync(cancellationToken);
     }
