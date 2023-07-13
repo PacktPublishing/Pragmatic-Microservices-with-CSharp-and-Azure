5c890c09e938b87c9768f254a83dc4a535c0d243
Christian Nagel <christian@christiannagel.com>
2023-07-13 14:49:28 +0200

    chapter 2 GamesQquery, CreateGameResponse, UpdateGame #14

---
+++ ch02/Codebreaker.GameAPIs.Models/Exceptions/CodebreakerException.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using Codebreaker.GameAPIs.Extensions;
+
+namespace Codebreaker.GameAPIs.Exceptions;
+
+public class CodebreakerException : Exception
+{
+    public string Code { get; set; } = string.Empty;
+
+	public CodebreakerException() { }
+	public CodebreakerException(string message) : base(message) { }
+	public CodebreakerException(string message, Exception inner) : base(message, inner) { }
+
+	public static void ThrowIfNull([NotNull] Game? game)
+	{
+        if (game is null)
+        {
+            throw new CodebreakerException("Game not found") { Code = CodebreakerExceptionCodes.GameNotFound };
+        }
+    }
+
+    public static void ThrowIfEnded(Game game)
+    {
+        if (game.Ended())
+        {
+            throw new CodebreakerException("Game is not active") { Code = CodebreakerExceptionCodes.GameNotActive };
+        }
+    }
+}
---
+++ ch02/Codebreaker.GameAPIs.Models/Exceptions/CodebreakerExceptionCodes.cs
@@ -0,0 +1,9 @@
+﻿namespace Codebreaker.GameAPIs.Exceptions;
+
+public class CodebreakerExceptionCodes
+{
+    public const string InvalidGameType = nameof(InvalidGameType);
+    public const string GameNotFound = nameof(GameNotFound);
+    public const string GameNotActive = nameof(GameNotActive);
+    public const string GameUpdateFailed = nameof(GameUpdateFailed);
+}
