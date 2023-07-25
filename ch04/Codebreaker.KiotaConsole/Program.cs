1a45818b967c52a7cb41505caf89bb37ff406529
Christian Nagel <christian@christiannagel.com>
2023-07-25 20:28:24 +0200

    kiota console app

---
+++ ch04/Codebreaker.KiotaConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+using Codebreaker.Client;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+//builder.Services.AddHttpClient<GamesClient>(client =>
+//{
+//    string gamesUrl = builder.Configuration["GamesApiUrl"] ?? throw new InvalidOperationException("GamesApiUrl not found");
+//    client.BaseAddress = new Uri(gamesUrl);
+//});
+
+builder.Services.Configure<RunnerOptions>(options =>
+{
+    options.GamesApiUrl = builder.Configuration["GamesApiUrl"] ?? throw new InvalidOperationException("GamesApiUrl not found");
+});
+
+builder.Services.AddTransient<Runner>();
+var app = builder.Build();
+
+var runner = app.Services.GetRequiredService<Runner>();
+await runner.RunAsync();
---
+++ ch04/Codebreaker.KiotaConsole/Runner.cs
@@ -0,0 +1,181 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Codebreaker.Client.Games;
+using Codebreaker.Client.Models;
+
+using Microsoft.Extensions.Options;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+using Spectre.Console;
+
+using static Codebreaker.Client.Games.GamesRequestBuilder;
+
+namespace Codebreaker.Client;
+
+internal class RunnerOptions
+{
+    public required string GamesApiUrl { get; set; }
+}
+
+internal class Runner
+{
+    private readonly GamesAPIClient _client;
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+    public Runner(IOptions<RunnerOptions> options)
+    {
+        AnonymousAuthenticationProvider authenticationProvider = new();
+        HttpClientRequestAdapter adapter = new(authenticationProvider)
+        {
+            BaseUrl = options.Value.GamesApiUrl ?? throw new InvalidOperationException("Could not read GamesApiUrl")
+        };
+        _client = new GamesAPIClient(adapter);
+    }
+
+    public async Task RunAsync()
+    {
+        bool ended = false;
+        while (!ended)
+        {
+            var selection = Inputs.GetMainSelection();
+            switch (selection)
+            {
+                case MainOptions.Play:
+                    await PlayGameAsync();
+                    break;
+                case MainOptions.Exit:
+                    ended = true;
+                    break;
+                case MainOptions.QueryGame:
+                    await ShowGameAsync();
+                    break;
+                case MainOptions.QueryList:
+                    await ShowGamesAsync();
+                    break;
+                case MainOptions.Delete:
+                    await DeleteGameAsync();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    private async Task DeleteGameAsync()
+    {
+        Guid gameId = Inputs.GetGameId();
+        await _client.Games[gameId.ToString()].DeleteAsync();
+    }
+
+    private async Task ShowGameAsync()
+    {
+        Guid gameId = Inputs.GetGameId();
+        var game = await _client.Games[gameId.ToString()].GetAsync(cancellationToken: _cancellationTokenSource.Token);
+        if (game is null)
+        {
+            await Console.Out.WriteLineAsync($"Game {gameId} not found");
+            return;
+        }
+        await Console.Out.WriteLineAsync($"Game found: {game}");
+        await Console.Out.WriteLineAsync($"last move: {game.LastMoveNumber}");
+
+        if (game.Moves is null)
+            return;
+
+        foreach (var move in game.Moves)
+        {
+            await Console.Out.WriteLineAsync(
+                $"{move.MoveNumber}. " +
+                $"{string.Join(':', move.GuessPegs ?? Enumerable.Empty<string>())} " +
+                $"{string.Join(':', move.KeyPegs ?? Enumerable.Empty<string>())}");
+        }
+        await Console.Out.WriteLineAsync();
+    }
+
+    private async Task ShowGamesAsync()
+    {
+        var games = await _client.Games.GetAsync(config =>
+        {
+            config.QueryParameters.Date = new Date(DateTime.Today);
+        }, _cancellationTokenSource.Token);
+        if (games is null)
+            throw new InvalidOperationException();
+
+        foreach (var game in games)
+        {
+            await Console.Out.WriteLineAsync($"{game.GameId}, {game.GameType}");
+        }
+        await Console.Out.WriteLineAsync();
+    }
+
+    private async Task PlayGameAsync()
+    {
+        GameType gameType = Inputs.GetGameType();
+        string playerName = Inputs.GetPlayername();
+
+        string[] ToStringArray(object o)
+        {
+            List<string> values = new();
+            if (o is JsonElement je)
+            {
+                foreach (var s in je.EnumerateArray())
+                {
+                    values.Add(s.GetString() ?? string.Empty);
+                }
+                return values.ToArray();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        (Guid gameId, int numberCodes, int maxMoves, IDictionary<string, string[]>? fields) = await AnsiConsole.Status().StartAsync<(Guid, int, int, IDictionary<string, string[]>)>("Starting game...", async _ =>
+        {
+            CreateGameRequest request = new()
+            {
+                PlayerName = playerName,
+                GameType = gameType
+            };
+            var response = await _client.Games.PostAsync(request, cancellationToken: _cancellationTokenSource.Token);
+            if (response is null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            IDictionary<string, string[]> fieldValues = response?.FieldValues?.AdditionalData
+            .ToDictionary(
+                entry => entry.Key,
+                entry => ToStringArray(entry.Value)) ?? throw new InvalidOperationException();
+
+            return (response.GameId!.Value, response.NumberCodes!.Value, response.MaxMoves!.Value, fieldValues);
+        });
+
+        int moveNumber = 0;
+        bool ended = false;
+        bool isVictory = false;
+        do
+        {
+            moveNumber++;
+            string[] guesses = Inputs.GetFieldChoices(numberCodes, fields);
+            await Console.Out.WriteAsync($"{moveNumber}. {string.Join(" ", guesses.Select(s => s.PadRight(8, ' ')))}");
+            UpdateGameRequest updateGameRequest = new()
+            {
+                MoveNumber = moveNumber,
+                GuessPegs = guesses.ToList()
+            };
+            UpdateGameResponse? response = await _client.Games[gameId.ToString()].PatchAsync(updateGameRequest, cancellationToken: _cancellationTokenSource.Token);
+            if (response is null)
+                throw new InvalidOperationException();
+
+            await Console.Out.WriteLineAsync($" ** {string.Join(' ', response.Results ?? Enumerable.Empty<string>())}");
+        } while (!ended);
+        await Console.Out.WriteLineAsync($"Victory: {isVictory}");
+        string wonOrLost = isVictory ? "won" : "lost";
+        await Console.Out.WriteLineAsync($"You {wonOrLost} after {moveNumber} moves");
+    }
+}
