21f4271957c7fe5b25916da877498ae74dbe67ba
Christian Nagel <christian@christiannagel.com>
2023-10-01 09:14:24 +0200

    server updates from chapter 3

--- ch04/server/Codebreaker.GameAPIs/Program.cs
+++ ch04/server/Codebreaker.GameAPIs/Program.cs
@@ -1,5 +1,10 @@
 using System.Runtime.CompilerServices;
 
+using Codebreaker.Data.Cosmos;
+using Codebreaker.Data.SqlServer;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 [assembly: InternalsVisibleTo("Codbreaker.APIs.Tests")]
@@ -14,7 +19,7 @@ builder.Services.AddSwaggerGen(options =>
     {
         Version = "v3",
         Title = "Codebreaker Games API",
-        Description = "An ASP.NET Core minimal API to play Codebreaker games",
+        Description = "An ASP.NET Core minimal APIs to play Codebreaker games",
         TermsOfService = new Uri("https://www.cninnovation.com/terms"),
         Contact = new OpenApiContact
         {
@@ -31,7 +36,32 @@ builder.Services.AddSwaggerGen(options =>
 
 // Application Services
 
-builder.Services.AddSingleton<IGamesRepository, GamesMemoryRepository>();
+string dataStorage = builder.Configuration["DataStorage"] ??= "Cosmos";
+
+if (dataStorage == "Cosmos")
+{
+    builder.Services.AddDbContext<IGamesRepository, GamesCosmosContext>(options =>
+    {
+        string connectionString = builder.Configuration.GetConnectionString("GamesCosmosConnection") ?? throw new InvalidOperationException("Could not find GamesCosmosConnection");
+        options.UseCosmos(connectionString, databaseName: "codebreaker")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+}
+else if (dataStorage == "SqlServer")
+{
+    builder.Services.AddDbContext<IGamesRepository, GamesSqlServerContext>(options =>
+    {
+        string connectionString = builder.Configuration.GetConnectionString("GamesSqlServerConnection") ?? throw new InvalidOperationException("Could not find GamesSqlServerConnection");
+        options.UseSqlServer(connectionString)
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    });
+
+}
+else
+{
+    builder.Services.AddSingleton<IGamesRepository, GamesMemoryRepository>();
+}
+
 builder.Services.AddScoped<IGamesService, GamesService>();
 
 var app = builder.Build();
