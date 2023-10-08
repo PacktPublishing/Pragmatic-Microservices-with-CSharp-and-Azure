71e8707a4e77e29bc2a686876c2a6ec19e1a385f
Christian Nagel <christian@christiannagel.com>
2023-10-08 16:07:21 +0200

    configuration prototype

---
+++ ch07/ConfigurationPrototype/Program.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+
+Console.WriteLine("Defined environment variables");
+IDictionary vars = Environment.GetEnvironmentVariables();
+foreach (object? key in vars.Keys)
+{
+    Console.WriteLine($"{key} {vars[key]}");
+}
+Console.WriteLine();
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("connectionstrings.json", optional: true);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+string[] summaries =
+[
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+];
+
+app.MapGet("/weatherforecast", (IConfiguration config) =>
+{
+    string? t = config["test"];
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
