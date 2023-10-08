e454bcb846543c41df7f3a9cd8da76f2d7c4ba3e
Christian Nagel <christian@christiannagel.com>
2023-10-08 23:15:09 +0200

    ch 6: azure app configuration

--- ch07/ConfigurationPrototype/Program.cs
+++ ch07/ConfigurationPrototype/Program.cs
@@ -11,7 +11,13 @@ Console.WriteLine();
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("connectionstrings.json", optional: true);
 
+string appConfigConnectionString = builder.Configuration["AzureAppConfigurationConnection"] ?? throw new InvalidOperationException("AzureAppConfigurationConnection not found");
+builder.Configuration.AddAzureAppConfiguration(appConfigConnectionString);
+
 // Add services to the container.
+
+builder.Services.Configure<Service1Options>(builder.Configuration.GetSection("Service1"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,30 +33,27 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-string[] summaries =
-[
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-];
-
-app.MapGet("/weatherforecast", (IConfiguration config) =>
+app.MapGet("/readconfig", (IConfiguration config) =>
 {
-    string? t = config["test"];
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    string? config1 = config["Config1"];
+    return $"config1: {config1}";
+});
+
+app.MapGet("/readoptions", (IOptions<Service1Options> options) =>
+{
+    return $"options - config1: {options.Value.Config1}; config 2: {options.Value.Config2}";
+});
+
+app.MapGet("/azureconfig", (IConfiguration config) =>
+{
+    string? connectionString = config.GetSection("ConfigurationPrototype").GetConnectionString("SqlServer");
+    return $"Configuration value from Azure App Configuration: {connectionString}";
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+internal class Service1Options
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public required string Config1 { get; set; }
+    public string? Config2 { get; set; }
 }
