c1e53b38b3b1fc723c2ff6a001d640be4d7c9730
Christian Nagel <christian@christiannagel.com>
2023-10-15 17:05:54 +0200

    initial sources from chapter 7

---
+++ ch08/Codebreaker.GameAPIs/Endpoints/GameEndpoints.cs
@@ -0,0 +1,164 @@
+﻿using Codebreaker.GameAPIs.Errors;
+
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Codebreaker.GameAPIs.Endpoints;
+
+public static class GameEndpoints
+{
+    public static void MapGameEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/games")
+            .WithTags("Games API");
+
+        group.MapPost("/", async Task<Results<Created<CreateGameResponse>, BadRequest<GameError>>> (
+            CreateGameRequest request,
+            IGamesService gameService,
+            HttpContext context,
+            CancellationToken cancellationToken) =>
+        {
+            Game game;
+            try
+            {
+                game = await gameService.StartGameAsync(request.GameType.ToString(), request.PlayerName, cancellationToken);
+            }
+            catch (CodebreakerException ex) when (ex.Code == CodebreakerExceptionCodes.InvalidGameType)
+            {
+                GameError error = new(ErrorCodes.InvalidGameType, $"Game type {request.GameType} does not exist", context.Request.GetDisplayUrl(),   Enum.GetNames<GameType>());
+                return TypedResults.BadRequest(error);
+            }
+            return TypedResults.Created($"/games/{game.GameId}", game.AsCreateGameResponse());
+        })
+        .WithName("CreateGame")
+        .WithSummary("Creates and starts a game")
+        .WithOpenApi(op =>
+        {
+            op.RequestBody.Description = "The game type and the player name of the game to create";
+            return op;
+        });
+
+        // Update the game resource with a move
+        group.MapPatch("/{gameId:guid}", async Task<Results<Ok<UpdateGameResponse>, NotFound, BadRequest<GameError>>> (
+            Guid gameId,
+            UpdateGameRequest request,
+            IGamesService gameService,
+            HttpContext context,
+            CancellationToken cancellationToken) =>
+        {
+            if (!request.End && request.GuessPegs == null)
+            {
+                return TypedResults.BadRequest(new GameError(ErrorCodes.InvalidMove, "End the game or set guesses", context.Request.GetDisplayUrl()));
+            }
+            try
+            {
+                if (request.End)
+                {
+                    Game? game = await gameService.EndGameAsync(gameId, cancellationToken);
+                    if (game is null)
+                        return TypedResults.NotFound();
+                    return TypedResults.Ok(game.AsUpdateGameResponse());
+                }
+                else
+                {
+                    (Game game, Move move) = await gameService.SetMoveAsync(gameId, request.GuessPegs!, request.MoveNumber, cancellationToken);
+                    return TypedResults.Ok(game.AsUpdateGameResponse(move.KeyPegs));
+                }
+            }
+            catch (ArgumentException ex) when (ex.HResult is >= 4200 and <= 4500)
+            {
+                string url = context.Request.GetDisplayUrl();
+                return ex.HResult switch
+                {
+                    4200 => TypedResults.BadRequest(new GameError(ErrorCodes.InvalidGuessNumber, "Invalid number of guesses received", url)),
+                    4300 => TypedResults.BadRequest(new GameError(ErrorCodes.UnexpectedMoveNumber, "Unexpected move number received", url)),
+                    4400 => TypedResults.BadRequest(new GameError(ErrorCodes.InvalidGuess, "Invalid guess values received!", url)),
+                    _ => TypedResults.BadRequest(new GameError(ErrorCodes.InvalidMove,"Invalid move received!", url))
+                };
+            }
+            catch (CodebreakerException ex) when (ex.Code == CodebreakerExceptionCodes.GameNotFound)
+            {
+                return TypedResults.NotFound();
+            }
+            catch (CodebreakerException ex) when (ex.Code == CodebreakerExceptionCodes.GameNotActive)
+            {
+                string url = context.Request.GetDisplayUrl();
+                return TypedResults.BadRequest(new GameError(ErrorCodes.GameNotActive, "The game already ended", url));
+            }
+        })
+        .WithName("SetMove")
+        .WithSummary("End the game or set a move")
+        .WithOpenApi(op =>
+        {
+            op.Parameters[0].Description = "The id of the game to set a move";
+            op.RequestBody.Description = "The data for creating the move";
+            return op;
+        });
+
+        // Get game by id
+        group.MapGet("/{gameId:guid}", async Task<Results<Ok<Game>, NotFound>> (
+            Guid gameId,
+            IGamesService gameService,
+            CancellationToken cancellationToken
+        ) =>
+        {
+            Game? game = await gameService.GetGameAsync(gameId, cancellationToken);
+
+            if (game is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            return TypedResults.Ok(game);
+        })
+        .WithName("GetGame")
+        .WithSummary("Gets a game by the given id")
+        .WithOpenApi(op =>
+        {
+            op.Parameters[0].Description = "The id of the game to get";
+            return op;
+        });
+
+        group.MapGet("/", async (
+            IGamesService gameService,
+            string? gameType = default,
+            string? playerName = default,
+            DateOnly? date = default,
+            bool ended = false,
+            CancellationToken cancellationToken = default) =>
+                {
+                    GamesQuery query = new(gameType, playerName, date, Ended: ended);
+                    var games = await gameService.GetGamesAsync(query, cancellationToken);
+                    return TypedResults.Ok(games);
+                })
+                .WithName("GetGames")
+                .WithSummary("Get games based on query parameters")
+                .WithOpenApi(op =>
+                {
+                    op.Parameters[0].Description = "The game type to filter by";
+                    op.Parameters[1].Description = "The player name to filter by";
+                    op.Parameters[2].Description = "The date to filter by";
+                    op.Parameters[3].Description = "Whether to filter by ended games";
+                    return op;
+                });
+
+        group.MapDelete("/{gameId:guid}", async (
+            Guid gameId,
+            IGamesService gameService, 
+            CancellationToken cancellationToken
+        ) =>
+        {
+            await gameService.DeleteGameAsync(gameId, cancellationToken);
+
+            return TypedResults.NoContent();
+        })
+        .WithName("DeleteGame")
+        .WithSummary("Deletes the game with the given id")
+        .WithDescription("Deletes a game from the database")
+        .WithOpenApi(op =>
+        {
+            op.Parameters[0].Description = "The id of the game to delete or cancel";
+            return op;
+        });
+    }
+}
---
+++ ch08/bicep/modules/containers/container-app.bicep
@@ -0,0 +1,104 @@
+metadata description = 'Creates an Azure Container App'
+
+// Parameters
+@description('The id of the container app environment')
+param containerAppEnvironmentId string
+
+@description('The name for the container app')
+@maxLength(16)
+param name string
+
+@description('Specifies the container image to deploy for the container app\nExample: \'mcr.microsoft.com/azuredocs/containerapps-helloworld:latest\'')
+param containerImage string = 'mcr.microsoft.com/azuredocs/containerapps-helloworld:latest'
+
+@description('The location for the container app')
+param location string = resourceGroup().location
+
+@description('Specifies the environment for resources.')
+@allowed([
+  'dev'
+  'test'
+  'qa'
+  'stage'
+  'prod'
+])
+param environment string = 'dev'
+
+@description('The target port for the container app')
+param targetPort int = 80
+
+@description('Number of CPU cores the container can use. Can be with a maximum of two decimals.')
+param cpu string = '0.25'
+
+@description('Amount of memory (in gibibytes, GiB) allocated to the container up to 4GiB. Can be with a maximum of two decimals. Ratio with CPU cores must be equal to 2.')
+param memory string = '0.5'
+
+@description('Minimum number of replicas the container app will be deployed')
+@minValue(0)
+@maxValue(300)
+param minReplicas int = 1
+
+@description('Maximum number of replicas the container app will be deployed')
+@minValue(1)
+@maxValue(300)
+param maxReplicas int = 3
+
+@description('Environment variables for the container app')
+param envVars array = []
+
+var regionCodes = {
+  centralus: 'cus'
+  southcentralus: 'scus'
+  northeurope: 'northeu'
+  westeurope: 'westeu'
+  australiacentral: 'auc'
+  southeastasia: 'seasia'
+}
+
+// remove space and make sure all lower case
+var sanitizedLocation = toLower(replace(location, ' ', ''))
+
+// get the region code
+var regionCode = contains(regionCodes, sanitizedLocation) ? regionCodes[sanitizedLocation] : sanitizedLocation
+
+// Resources
+resource containerApp 'Microsoft.App/containerApps@2023-05-01' = {
+  name: '${name}-${regionCode}-${environment}'
+  location: location
+  properties: {
+    managedEnvironmentId: containerAppEnvironmentId
+    configuration: {
+      ingress: {
+        external: true
+        targetPort: targetPort
+        allowInsecure: false
+        traffic: [
+          {
+            latestRevision: true
+            weight: 100
+          }
+        ]
+      }
+    }
+    template: {
+      containers: [
+        {
+          name: name
+          image: containerImage
+          env: envVars
+          resources: {
+            cpu: json(cpu)  // Convert cpu to a number with the json() function
+            memory: '${memory}Gi'
+          }
+        }
+      ]
+      scale: {
+        minReplicas: minReplicas
+        maxReplicas: maxReplicas
+      }
+    }
+  }
+}
+
+// Outputs
+output name string = containerApp.name
