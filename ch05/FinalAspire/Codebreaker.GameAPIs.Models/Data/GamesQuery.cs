adc63090e2f33b5a4ec75881dd742a96687e4208
Christian Nagel <christian@christiannagel.com>
2023-12-28 23:06:33 +0100

    cleanup

--- ch05/FinalAspire/Codebreaker.GameAPIs.Models/Data/GamesQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Codebreaker.GameAPIs.Data;
-
-public record GamesQuery(
-    string? GameType = default, 
-    string? PlayerName = default, 
-    DateOnly? Date = default,
-    bool Ended = true,
-    bool RunningOnly = false);
