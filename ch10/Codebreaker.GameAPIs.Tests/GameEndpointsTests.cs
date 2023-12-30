1efb33f8272cab2bd9d093c0f92b7a92a57ad725
Christian Nagel <christian@christiannagel.com>
2023-12-30 23:07:02 +0100

    projects with initial tests

---
+++ ch10/Codebreaker.GameAPIs.Tests/GameEndpointsTests.cs
@@ -0,0 +1,81 @@
+using System.Net;
+using System.Net.Http.Json;
+
+using Codebreaker.GameAPIs.Models;
+
+namespace Codebreaker.GameAPIs.Tests;
+
+public class GameEndpointsTests
+{
+    [Fact]
+    public async Task SetMoveWithInvalidMoveNumberShouldReturnBadRequest()
+    {
+        await using GamesApiApplication app = new();
+        var client = app.CreateClient();
+
+        int moveNumber = 0;
+        CreateGameResponse gameResponse = await StartGameFixtureAsync(app);
+
+        UpdateGameRequest updateGameRequest = new(gameResponse.Id, gameResponse.GameType, gameResponse.PlayerName, moveNumber)
+        {
+            GuessPegs = ["Red", "Red", "Red", "Red"]
+        };
+
+        string uri = $"/games/{updateGameRequest.Id}";
+        var updateGameResponse = await client.PatchAsJsonAsync(uri, updateGameRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, updateGameResponse.StatusCode);       
+    }
+
+    [Fact]
+    public async Task SetMoveWithInvalidGuessNumberShouldReturnBadRequest()
+    {
+        await using GamesApiApplication app = new();
+        var client = app.CreateClient();
+
+        int moveNumber = 1;
+        CreateGameResponse gameResponse = await StartGameFixtureAsync(app);
+
+        UpdateGameRequest updateGameRequest = new(gameResponse.Id, gameResponse.GameType, gameResponse.PlayerName, moveNumber)
+        {
+            GuessPegs = ["Red", "Red", "Red",]
+        };
+
+        string uri = $"/games/{updateGameRequest.Id}";
+        var updateGameResponse = await client.PatchAsJsonAsync(uri, updateGameRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, updateGameResponse.StatusCode);
+    }
+
+    [Fact]
+    public async Task SetMoveWithWrongGuessesShouldReturnBadRequest()
+    {
+        await using GamesApiApplication app = new();
+        var client = app.CreateClient();
+
+        int moveNumber = 1;
+        CreateGameResponse gameResponse = await StartGameFixtureAsync(app);
+
+        UpdateGameRequest updateGameRequest = new(gameResponse.Id, gameResponse.GameType, gameResponse.PlayerName, moveNumber)
+        {
+            GuessPegs = ["Red", "Red", "Red", "Schwarz"]
+        };
+
+        string uri = $"/games/{updateGameRequest.Id}";
+        var updateGameResponse = await client.PatchAsJsonAsync(uri, updateGameRequest);
+
+        Assert.Equal(HttpStatusCode.BadRequest, updateGameResponse.StatusCode);
+    }
+
+    private async Task<CreateGameResponse> StartGameFixtureAsync(GamesApiApplication app)
+    {
+        var client = app.CreateClient();
+        CreateGameRequest request = new(GameType.Game6x4, "test");
+        var response = await client.PostAsJsonAsync("/games", request);
+        var gameReponse = await response.Content.ReadFromJsonAsync<CreateGameResponse>();
+
+        if (gameReponse is null)
+            Assert.Fail("gameResponse is null");
+        return gameReponse;
+    }
+}
---
+++ ch10/Codebreaker.GameAPIs.Tests/GamesApiApplication.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace Codebreaker.GameAPIs.Tests;
+
+internal class GamesApiApplication(string environment = "Development") : WebApplicationFactory<Program>
+{
+    private readonly string _environment = environment;
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostOptions(webBuilder =>
+        {
+        });
+
+        builder.UseEnvironment(_environment);
+
+        builder.ConfigureServices(services =>
+        {
+                
+        });
+
+        Environment.SetEnvironmentVariable("DataStorage", "InMemory");
+        return base.CreateHost(builder);
+    }
+}
+
