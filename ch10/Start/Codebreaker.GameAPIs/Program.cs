70c77d8cd5dc935aa2f85528dee300da23ab7ad7
Christian Nagel <christian@christiannagel.com>
2024-01-04 13:38:59 +0100

    chapter 10 start projects

---
+++ ch10/Start/Codebreaker.GameAPIs/Program.cs
@@ -0,0 +1,69 @@
+using Codebreaker.Data.SqlServer;
+using Codebreaker.GameAPIs;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+builder.AddAppConfiguration();
+
+// Swagger/EndpointDocumentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v3", new OpenApiInfo
+    {
+        Version = "v3",
+        Title = "Codebreaker Games API",
+        Description = "An ASP.NET Core minimal APIs to play Codebreaker games",
+        TermsOfService = new Uri("https://www.cninnovation.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Christian Nagel",
+            Url = new Uri("https://csharp.christiannagel.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License API Usage",
+            Url = new Uri("https://www.cninnovation.com/apiusage")
+        }
+    });
+});
+
+// Application Services
+
+builder.AddApplicationServices();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v3/swagger.json", "v3");
+});
+
+if (builder.Configuration["DataStore"] == "SqlServer" && builder.Environment.IsDevelopment())
+{
+    try
+    {
+        using var scope = app.Services.CreateScope();
+        var repo = scope.ServiceProvider.GetRequiredService<GamesSqlServerContext>();
+        if (repo is GamesSqlServerContext context)
+        {
+            await context.Database.MigrateAsync();
+            app.Logger.LogInformation("Database updated");
+        }
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "Error updating database");
+    }
+}
+
+app.MapGameEndpoints();
+
+app.Run();
