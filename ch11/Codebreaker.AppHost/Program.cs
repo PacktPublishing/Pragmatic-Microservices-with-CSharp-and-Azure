6967e5f3e68bfad43fcb2ed6a419b736170f1b56
Christian Nagel <christian@christiannagel.com>
2024-01-06 11:26:06 +0100

    initial grafana

--- ch11/Codebreaker.AppHost/Program.cs
+++ ch11/Codebreaker.AppHost/Program.cs
@@ -1,5 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+                     .WithVolumeMount("../grafana/config", "/etc/grafana")
+                     .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+                     .WithServiceBinding(containerPort: 3000, hostPort: 3000, name: "grafana-http", scheme: "http");
+
 builder.AddContainer("prometheus", "prom/prometheus")
        .WithVolumeMount("../prometheus", "/etc/prometheus")
        .WithServiceBinding(9090, hostPort: 9090);
@@ -12,7 +17,8 @@ var cosmos = builder.AddAzureCosmosDB("GamesCosmosConnection", cosmosConnectionS
 
 var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
     .WithReference(cosmos)
-    .WithReference(appConfiguration);
+    .WithReference(appConfiguration)
+    .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"));
 
 builder.AddProject<Projects.CodeBreaker_Bot>("bot")
     .WithReference(gameAPIs)
