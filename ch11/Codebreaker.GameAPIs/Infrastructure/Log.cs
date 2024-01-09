e855c0c9dae3afa2e46029c6d71f66418d3b345b
Christian Nagel <christian@christiannagel.com>
2024-01-09 08:55:52 +0100

    GamesService with logging and metrics

--- ch11/Codebreaker.GameAPIs/Infrastructure/Log.cs
+++ ch11/Codebreaker.GameAPIs/Infrastructure/Log.cs
@@ -5,14 +5,26 @@ public static partial class Log
     [LoggerMessage(
         EventId = 3000,
         Level = LogLevel.Error,
-        Message = "{message}")]
-    public static partial void Error(this ILogger logger, Exception ex, string message);
+        Message = "{error}")]
+    public static partial void Error(this ILogger logger, Exception ex, string error);
+
+    [LoggerMessage(
+               EventId = 3001,
+               Level = LogLevel.Warning,
+               Message = "Game {gameId} not found")]
+    public static partial void GameNotFound(this ILogger logger, Guid gameId);
+
+    [LoggerMessage(
+        EventId = 3002,
+        Level = LogLevel.Warning,
+        Message = "Invalid move received {gameId}, guesses: {guesses}, {error}")]
+    public static partial void InvalidMoveReceived(this ILogger logger, Guid gameId, string guesses, string error);
 
     [LoggerMessage(
         EventId = 4000,
         Level = LogLevel.Information,
-        Message = "Sending the move {move} to {game}")]
-    public static partial void SendMove(this ILogger logger, string move, string game);
+        Message = "The move {move} was set for {game} with result {result}")]
+    public static partial void SendMove(this ILogger logger, string move, string game, string result);
 
     [LoggerMessage(
         EventId = 4001,
@@ -20,39 +32,4 @@ public static partial class Log
         Message = "Matched after {count} moves with {game}")]
     public static partial void Matched(this ILogger logger, int count, string game);
 
-    [LoggerMessage(
-        EventId = 4002,
-        Level = LogLevel.Information,
-        Message = "Reduced the possible values to {number} with {color} hits in {game}")]
-    public static partial void ReducedPossibleValues(this ILogger logger, int number, string color, string game);
-
-    [LoggerMessage(
-        EventId = 4003,
-        Level = LogLevel.Information,
-        Message = "Finished run with {number} in {game}")]
-    public static partial void FinishedRun(this ILogger logger, int number, string game);
-
-    [LoggerMessage(
-        EventId = 4004,
-        Level = LogLevel.Information,
-        Message = "Using URI {uri} to access the API service")]
-    public static partial void UsingUri(this ILogger logger, string uri);
-
-    [LoggerMessage(
-        EventId = 4005,
-        Level = LogLevel.Information,
-        Message = "Start CodeBreakerGameRunner")]
-    public static partial void StartGameRunner(this ILogger logger);
-
-    [LoggerMessage
-        (EventId = 4006,
-        Level = LogLevel.Trace,
-        Message = "Waiting for next timer tick in loop {loop}")]
-    public static partial void WaitingForNextTick(this ILogger logger, int loop);
-
-    [LoggerMessage
-        (EventId = 4007,
-        Level = LogLevel.Trace,
-        Message = "Timer tick fired in loop {loop}")]
-    public static partial void TimerTickFired(this ILogger logger, int loop);
 }
--- ch11/Codebreaker.GameAPIs/Services/GamesService.cs
+++ ch11/Codebreaker.GameAPIs/Services/GamesService.cs
@@ -7,7 +7,7 @@ public class GamesService(IGamesRepository dataRepository, ILogger<GamesService>
     public async Task<Game> StartGameAsync(string gameType, string playerName, CancellationToken cancellationToken = default)
     {
         Game game = GamesFactory.CreateGame(gameType, playerName);
-        using var activity = Tracing.StartGame(game.Id, game.GameType);
+        using var activity = GamesActivity.StartGame(game.Id, game.GameType);
 
         await dataRepository.AddGameAsync(game, cancellationToken);
         metrics.GameStarted(game);
@@ -16,20 +16,36 @@ public class GamesService(IGamesRepository dataRepository, ILogger<GamesService>
 
     public async Task<(Game Game, Move Move)> SetMoveAsync(Guid id, string gameType, string[] guesses, int moveNumber, CancellationToken cancellationToken = default)
     {
-        Game? game = await dataRepository.GetGameAsync(id, cancellationToken);
-        CodebreakerException.ThrowIfNull(game);
-        CodebreakerException.ThrowIfEnded(game);
-        CodebreakerException.ThrowIfUnexpectedGameType(game, gameType);
-        using var activity = Tracing.SetMove(game.Id, game.GameType);
-
-        Move move = game.ApplyMove(guesses, moveNumber);
-
-        // Update the game in the game-service database
-        await dataRepository.AddMoveAsync(game, move, cancellationToken);
-        metrics.MoveSet(game.Id, DateTime.UtcNow, game.GameType);
-        if (game.Ended())
+        Game? game = default;
+        Move? move;
+        try
         {
-            metrics.GameEnded(game); 
+            game = await dataRepository.GetGameAsync(id, cancellationToken);
+            CodebreakerException.ThrowIfNull(game);
+            CodebreakerException.ThrowIfEnded(game);
+            CodebreakerException.ThrowIfUnexpectedGameType(game, gameType);
+            using var activity = GamesActivity.SetMove(game.Id, game.GameType);
+
+            move = game.ApplyMove(guesses, moveNumber);
+
+            // Update the game in the game-service database
+            await dataRepository.AddMoveAsync(game, move, cancellationToken);
+            metrics.MoveSet(game.Id, DateTime.UtcNow, game.GameType);
+            if (game.Ended())
+            {
+                metrics.GameEnded(game);
+            }
+        }
+        catch (ArgumentException ex)
+        {
+            logger.InvalidMoveReceived(game?.Id ?? Guid.Empty, string.Join(':', guesses), ex.Message);
+            metrics.InvalidMove();
+            throw;
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, ex.Message);
+            throw;
         }
         return (game, move);
     }
