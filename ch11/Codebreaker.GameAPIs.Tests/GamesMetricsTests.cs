03ac6e919fc03f3e0c10362706313c51fbb1ca6f
Christian Nagel <christian@christiannagel.com>
2024-01-09 08:51:49 +0100

    update unit tests for metrics

---
+++ ch11/Codebreaker.GameAPIs.Tests/GamesMetricsTests.cs
@@ -0,0 +1,131 @@
+﻿using Codebreaker.GameAPIs.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.Metrics.Testing;
+using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+using System.Diagnostics.Metrics;
+
+namespace Codebreaker.GameAPIs.Tests;
+
+public class GamesMetricsTests
+{
+    private Guid _gameId = Guid.Parse("DBDF4DD9-3A02-4B2A-87F6-FFE4BA1DCE52");
+    private DateTime _gameStartTime = new DateTime(2024, 1, 1, 12, 10, 5);
+    private DateTime _gameMove1Time = new DateTime(2024, 1, 1, 12, 10, 15);
+    private DateTime _gameMove2Time = new DateTime(2024, 1, 1, 12, 10, 30);
+
+    [Fact]
+    public void GameStarted_Should_IncrementActiveGamesCounter()
+    {
+        // arrange
+        (IMeterFactory meterFactory, GamesMetrics metrics) = CreateMeterFactorySkeleton();
+        var collector = new MetricCollector<long>(meterFactory, GamesMetrics.MeterName, "codebreaker.active_games");
+        var game = GetGame();
+
+        // act
+        metrics.GameStarted(game);
+
+        // assert
+        var measurements = collector.GetMeasurementSnapshot();
+        Assert.Single(measurements);
+        Assert.Equal(1, measurements[0].Value);
+    }
+
+    [Fact]
+    public void MoveSet_Should_Record_ThinkTime()
+    {
+        // arrange
+        (IMeterFactory meterFactory, GamesMetrics metrics) = CreateMeterFactorySkeleton();
+        var collector = new MetricCollector<double>(meterFactory, GamesMetrics.MeterName, "codebreaker.move_think_time");
+        var game = GetGame();
+        metrics.GameStarted(game);
+
+        // act
+        metrics.MoveSet(game.Id, _gameMove1Time, "Game6x4");
+
+        // assert
+        var measurements = collector.GetMeasurementSnapshot();
+        Assert.Single(measurements);
+        Assert.Equal(10, measurements[0].Value);
+    }
+
+    [Fact]
+    public void GameEnded_Should_RecordValues()
+    {
+        // arrange
+        (IMeterFactory meterFactory, GamesMetrics metrics) = CreateMeterFactorySkeleton();
+        var gameDurationCollector = new MetricCollector<double>(meterFactory, GamesMetrics.MeterName, "codebreaker.game_duration");
+        var activeGamesCollector = new MetricCollector<long>(meterFactory, GamesMetrics.MeterName, "codebreaker.active_games");
+        var movesPerGameWinCollector = new MetricCollector<int>(meterFactory, GamesMetrics.MeterName, "codebreaker.game_moves-per-win");
+        var gamesWonCollector = new MetricCollector<long>(meterFactory, GamesMetrics.MeterName, "codebreaker.games.won");
+        var gamesLostCollector = new MetricCollector<long>(meterFactory, GamesMetrics.MeterName, "codebreaker.games.lost");
+
+        var game = GetGame();
+        metrics.GameStarted(game);
+        metrics.MoveSet(game.Id, _gameMove1Time, "Game6x4");
+        metrics.MoveSet(game.Id, _gameMove2Time, "Game6x4");
+
+        // act
+        game.EndTime = _gameMove2Time;
+        game.IsVictory = true;
+        game.LastMoveNumber = 2;
+        game.Duration = TimeSpan.FromSeconds(25);
+        metrics.GameEnded(game);
+
+        // assert
+        var gameDurationMeasurements = gameDurationCollector.GetMeasurementSnapshot();
+        Assert.Single(gameDurationMeasurements);
+        Assert.Equal(25, gameDurationMeasurements[0].Value);
+
+        var activeGamesMeasurements = activeGamesCollector.GetMeasurementSnapshot();
+        Assert.Equal(2, activeGamesMeasurements.Count);
+        Assert.Equal(-1, activeGamesMeasurements.Last().Value);
+
+        var movesPerGameWinMeasurements = movesPerGameWinCollector.GetMeasurementSnapshot();
+        Assert.Single(movesPerGameWinMeasurements);
+        Assert.Equal(2, movesPerGameWinMeasurements[0].Value);
+
+        var gamesWonMeasurements = gamesWonCollector.GetMeasurementSnapshot();
+        Assert.Single(gamesWonMeasurements);
+        Assert.Equal(1, gamesWonMeasurements[0].Value);
+
+        var gamesLostMeasurements = gamesLostCollector.GetMeasurementSnapshot();
+        Assert.Empty(gamesLostMeasurements);       
+    }
+
+    static void OnMeasurementRecorded<T>(
+        Instrument instrument,
+        T measurement,
+        ReadOnlySpan<KeyValuePair<string, object?>> tags,
+        object? state)
+    {
+        Console.WriteLine($"{instrument.Name} recorded measurement {measurement}");
+    }
+
+    private static IServiceProvider CreateServiceProvider()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddMetrics();
+        serviceCollection.AddSingleton<GamesMetrics>();
+        return serviceCollection.BuildServiceProvider();
+    }
+
+    private static (IMeterFactory MeterFactory, GamesMetrics Metrics) CreateMeterFactorySkeleton()
+    {
+        var services = CreateServiceProvider();
+        GamesMetrics metrics = services.GetRequiredService<GamesMetrics>();
+        IMeterFactory meterFactory = services.GetRequiredService<IMeterFactory>();
+        return (meterFactory, metrics);
+    }
+
+    public Game GetGame()
+    {
+        return new Game(_gameId, "Game6x4", "Test", _gameStartTime, 4, 12)
+        {
+            Codes = ["Red", "Green", "Blue", "Yellow"],
+            FieldValues = new Dictionary<string, IEnumerable<string>>()
+            {
+                { FieldCategories.Colors, ["Red", "Green", "Blue", "Yellow", "Purple", "Orange"] }
+            }
+        };
+    }
+}
---
+++ ch11/Codebreaker.GameAPIs.Tests/TestMeterFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Codebreaker.GameAPIs.Tests;
+
+internal sealed class TestMeterFactory : IMeterFactory
+{
+    public List<Meter> Meters { get; } = new List<Meter>();
+
+    public Meter Create(MeterOptions options)
+    {
+        var meter = new Meter(options.Name, options.Version, Array.Empty<KeyValuePair<string, object?>>(), scope: this);
+        Meters.Add(meter);
+        return meter;
+    }
+
+    public void Dispose()
+    {
+        foreach (var meter in Meters)
+        {
+            meter.Dispose();
+        }
+
+        Meters.Clear();
+    }
+}
