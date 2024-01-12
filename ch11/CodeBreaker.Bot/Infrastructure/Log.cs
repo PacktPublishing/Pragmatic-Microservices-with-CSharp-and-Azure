b0797db38c9fcae1882d7a7ada328874c99dd65c
Christian Nagel <christian@christiannagel.com>
2024-01-12 12:50:22 +0100

    update logs move to Infrastructure template placeholders uppercase change game id to Guid with logs

--- ch11/CodeBreaker.Bot/Infrastructure/Log.cs
+++ ch11/CodeBreaker.Bot/Infrastructure/Log.cs
@@ -1,41 +1,41 @@
 ﻿namespace CodeBreaker.Bot;
 
-public static partial class Log
+internal static partial class Log
 {
     [LoggerMessage(
         EventId = 3000,
         Level = LogLevel.Error,
-        Message = "{message}")]
-    public static partial void Error(this ILogger logger, Exception ex, string message);
+        Message = "{ErrorMessage}")]
+    public static partial void Error(this ILogger logger, Exception ex, string errorMessage);
 
     [LoggerMessage(
         EventId = 4000,
         Level = LogLevel.Information,
-        Message = "Sending the move {move} to {game}")]
-    public static partial void SendMove(this ILogger logger, string move, string game);
+        Message = "Sending the move {Move} to {GameId}")]
+    public static partial void SendMove(this ILogger logger, string move, Guid gameId);
 
     [LoggerMessage(
         EventId = 4001,
         Level = LogLevel.Information,
-        Message = "Matched after {count} moves with {game}")]
-    public static partial void Matched(this ILogger logger, int count, string game);
+        Message = "Matched after {NumberMoves} moves with {GameId}")]
+    public static partial void Matched(this ILogger logger, int numberMoves, string gameId);
 
     [LoggerMessage(
         EventId = 4002,
         Level = LogLevel.Information,
-        Message = "Reduced the possible values to {number} with {color} hits in {game}")]
-    public static partial void ReducedPossibleValues(this ILogger logger, int number, string color, string game);
+        Message = "Reduced the possible values to {Number} with {Color} hits in {GameId}")]
+    public static partial void ReducedPossibleValues(this ILogger logger, int number, string color, Guid gameId);
 
     [LoggerMessage(
         EventId = 4003,
         Level = LogLevel.Information,
-        Message = "Finished run with {number} in {game}")]
-    public static partial void FinishedRun(this ILogger logger, int number, string game);
+        Message = "Finished run with {MoveNumber} in {GameId}")]
+    public static partial void FinishedRun(this ILogger logger, int moveNumber, Guid gameId);
 
     [LoggerMessage(
         EventId = 4004,
         Level = LogLevel.Information,
-        Message = "Using URI {uri} to access the API service")]
+        Message = "Using URI {Uri} to access the API service")]
     public static partial void UsingUri(this ILogger logger, string uri);
 
     [LoggerMessage(
@@ -47,12 +47,12 @@ public static partial class Log
     [LoggerMessage
         (EventId = 4006,
         Level = LogLevel.Trace,
-        Message = "Waiting for next timer tick in loop {loop}")]
+        Message = "Waiting for next timer tick in loop {Loop}")]
     public static partial void WaitingForNextTick(this ILogger logger, int loop);
 
     [LoggerMessage
     (EventId = 4007,
     Level = LogLevel.Trace,
-    Message = "Timer tick fired in loop {loop}")]
+    Message = "Timer tick fired in loop {Loop}")]
     public static partial void TimerTickFired(this ILogger logger, int loop);
 }
