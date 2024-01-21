0e9577ebf7c923aeeaf49b0113cb37a8705e931e
Christian Nagel <christian@christiannagel.com>
2024-01-21 21:54:05 +0100

    remove not needed projects from chapter 11

--- ch12/Codebreaker.GameAPIs.Client.Tests/Usings.cs
+++
@@ -1,9 +0,0 @@
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.Logging.Abstractions;
-
-global using Moq;
-global using Moq.Protected;
-
-global using System.Net;
-
-global using Xunit;
