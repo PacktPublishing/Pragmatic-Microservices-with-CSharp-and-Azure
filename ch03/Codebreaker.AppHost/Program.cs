7d21349e9f0442698d90bbc9f6cc799b2d05b4a0
Christian Nagel <christian@christiannagel.com>
2024-02-18 11:28:54 +0100

    chapter 3 bump packages #65

--- ch03/Codebreaker.AppHost/Program.cs
+++ ch03/Codebreaker.AppHost/Program.cs
@@ -7,7 +7,7 @@ string dataStore = builder.Configuration["DataStore"] ?? "InMemory";
 var sqlServerConnectionString = builder.Configuration.GetConnectionString("GamesSqlServerConnection") ?? throw new InvalidOperationException("Could not read GamesSqlServerConnection");
 var cosmosConnectionString = builder.Configuration.GetConnectionString("GamesCosmosConnection") ?? throw new InvalidOperationException("Could not read GamesCosmosConnection");
 
-builder.AddProject<Projects.Codebreaker_GameAPIs>("codebreaker.gameapis")
+builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
     .WithEnvironment("DataStore", dataStore)
     .WithEnvironment("ConnectionStrings__GamesSqlServerConnection", sqlServerConnectionString)
     .WithEnvironment("ConnectionStrings__GamesCosmosConnection", cosmosConnectionString);
