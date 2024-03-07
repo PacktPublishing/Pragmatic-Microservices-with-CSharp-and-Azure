1bda4e70c45eec95904d0d7d75c7c2331c4ee8e2
Christian Nagel <christian@christiannagel.com>
2024-03-07 19:46:58 +0100

    ch12 preview 4 updates

--- ch12/Codebreaker.AppHost/Program.cs
+++ ch12/Codebreaker.AppHost/Program.cs
@@ -1,6 +1,7 @@
-
 using Codebreaker.AppHost;
 using Codebreaker.ServiceDefaults;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -11,20 +12,20 @@ if (builder.Environment.IsPrometheus())
     builder.AddUserSecretsForPrometheusEnvironment();
     string sqlPassword = builder.Configuration["SqlPassword"] ?? throw new InvalidOperationException("could not read password");
 
-    var sqlServer = builder.AddSqlServerContainer("sql", sqlPassword)
-        .WithVolumeMount("volume.codebreaker.sql", "/var/opt/mssql", VolumeMountType.Named)
-        .AddDatabase("CodebreakerSql");
+    var sqlServer = builder.AddSqlServer("sql", sqlPassword)
+        .WithVolume("volume.codebreaker.sql", "/var/opt/mssql", isReadOnly: false)
+        .AddDatabase("CodebreakerSql", "codebreaker");
 
     var prometheus = builder.AddContainer("prometheus", "prom/prometheus")
-           .WithVolumeMount("../prometheus", "/etc/prometheus")
-           .WithHttpEndpoint(containerPort: 9090, hostPort: 9090);
+        .WithBindMount("../prometheus", "/etc/prometheus")
+        .WithHttpEndpoint(containerPort: 9090, hostPort: 9090);
 
     var grafana = builder.AddContainer("grafana", "grafana/grafana")
-                         .WithVolumeMount("../grafana/config", "/etc/grafana")
-                         .WithVolumeMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
-                         .WithHttpEndpoint(containerPort: 3000, hostPort: 3000, name: "grafana-http");
+        .WithBindMount("../grafana/config", "/etc/grafana")
+        .WithBindMount("../grafana/dashboards", "/var/lib/grafana/dashboards")
+        .WithHttpEndpoint(containerPort: 3000, hostPort: 3000, name: "grafana-http");
 
-    var redis = builder.AddRedisContainer("redis");
+    var redis = builder.AddRedis("redis");
 
     var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
         .WithReference(sqlServer)
@@ -33,48 +34,55 @@ if (builder.Environment.IsPrometheus())
         .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"))
         .WithReplicas(1);
 
-    builder.AddProject<Projects.CodeBreaker_Blazor_Host>("blazor")
-        .WithReference(gameAPIs);
+    //builder.AddProject<Projects.CodeBreaker_Blazor_Host>("blazor")
+    //    .WithReference(gameAPIs);
 
     builder.AddProject<Projects.CodeBreaker_Bot>("bot")
         .WithReference(gameAPIs);
-
 }
 else
 {
-    var appInsightsConnectionString = builder.Configuration["ApplicationInsightsConnectionString"] ?? throw new InvalidOperationException("Could not read AppInsightsConnectionString");
+    var redis = builder.AddRedis("redis");
 
-    var redis = builder.AddRedisContainer("redis");
+    IResourceBuilder<ProjectResource>? gameAPIs;
 
-    //string sqlPassword = builder.Configuration["SqlPassword"] ?? throw new InvalidOperationException("could not read password");
-    //var sqlServer = builder.AddSqlServerContainer("sql", sqlPassword)
-    //    .WithVolumeMount("volume.codebreaker.sql", "/var/opt/mssql", VolumeMountType.Named)
-    //    .AddDatabase("CodebreakerSql");
+    // The following is an alternative option running locally by using a connection to an existing Azure App Insights resource and a connection to Azure Cosmos DB with the local emulator running (link is configured with appsettings.development.json)
+    //string appInsightsConnection = builder.Configuration["ApplicationInsightsConnectionString"] ?? throw new InvalidOperationException("Configure the ApplicationInsightsConnectionString key within user secrets to reference your App Insights Azure resource");
+    //string cosmosConnection = builder.Configuration.GetConnectionString("GamesCosmosConnection") ?? throw new InvalidOperationException("Could not read CosmosConnection");
+    //gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
+    //    .WithReference(redis)
+    //    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", appInsightsConnection)
+    //    .WithEnvironment("DataStore", dataStore)
+    //    .WithEnvironment("ConnectionStrings__cosmos", cosmosConnection)
+    //    .WithReplicas(1);
 
-//#if DEBUG
-//    var cosmos = builder.AddAzureCosmosDB("codebreakercosmos")
-//        .UseEmulator(port: 8081)
-//        .AddDatabase("codebreaker")
-//        .WithHttpEndpoint(8081, hostPort: 8082, name: "cosmos");
-//#else
-    var cosmos = builder.AddAzureCosmosDB("codebreakercosmos")
+    //builder.AddProject<Projects.CodeBreaker_Blazor_Host>("blazor")
+    //    .WithReference(gameAPIs)
+    //    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", appInsightsConnection);
+
+    //builder.AddProject<Projects.CodeBreaker_Bot>("bot")
+    //    .WithReference(gameAPIs)
+    //    .WithEnvironment("APPLICATIONINSIGHTS_CONNECTION_STRING", appInsightsConnection);
+
+    var appInsights = builder.AddAzureApplicationInsights("AppInsights");
+
+    var cosmos = builder.AddAzureCosmosDB("cosmos")
         .AddDatabase("codebreaker");
-//#endif
 
-    var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
+    gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
         .WithReference(cosmos)
         .WithReference(redis)
+        .WithReference(appInsights)
         .WithEnvironment("DataStore", dataStore)
-        .WithEnvironment("ApplicationInsightsConnectionString", appInsightsConnectionString)
         .WithReplicas(1);
 
     builder.AddProject<Projects.CodeBreaker_Blazor_Host>("blazor")
         .WithReference(gameAPIs)
-        .WithEnvironment("ApplicationInsightsConnectionString", appInsightsConnectionString);
+        .WithReference(appInsights);
 
     builder.AddProject<Projects.CodeBreaker_Bot>("bot")
         .WithReference(gameAPIs)
-        .WithEnvironment("ApplicationInsightsConnectionString", appInsightsConnectionString);
+        .WithReference(appInsights);
 }
 
 builder.Build().Run();
