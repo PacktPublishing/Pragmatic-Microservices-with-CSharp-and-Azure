fcaa4c2ccc2fe043035f36939cb40f595a615cd4
Christian Nagel <christian@christiannagel.com>
2024-03-11 09:56:05 -0700

    kiota code generation updates

--- ch04/client/Codebreaker.GameAPIs.KiotaClient/codebreaker/Models/UpdateGameResponse.cs
+++ ch04/client/Codebreaker.GameAPIs.KiotaClient/codebreaker/Models/UpdateGameResponse.cs
@@ -27,6 +27,7 @@ namespace Codebreaker.Client.Models {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
+        /// <returns>A <see cref="UpdateGameResponse"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
         public static UpdateGameResponse CreateFromDiscriminatorValue(IParseNode parseNode) {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
@@ -35,6 +36,7 @@ namespace Codebreaker.Client.Models {
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
+        /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
         public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers() {
             return new Dictionary<string, Action<IParseNode>> {
                 {"ended", n => { Ended = n.GetBoolValue(); } },
