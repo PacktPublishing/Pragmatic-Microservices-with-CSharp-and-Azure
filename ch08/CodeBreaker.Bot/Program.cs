e1af16eaca57e0d5fac214277054615b17c2f7b1
Christian Nagel <christian@christiannagel.com>
2024-03-30 22:20:24 +0100

    aspire updates

--- ch08/CodeBreaker.Bot/Program.cs
+++ ch08/CodeBreaker.Bot/Program.cs
@@ -8,28 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-builder.Configuration.AddAzureAppConfiguration(appConfigOptions =>
-{
-#if DEBUG
-    DefaultAzureCredential credential = new();
-#else
-    string managedIdentityClientId = builder.Configuration["AZURE_CLIENT_ID"] ?? string.Empty;
-    DefaultAzureCredentialOptions credentialOptions = new()
-    {
-        ManagedIdentityClientId = managedIdentityClientId,
-        ExcludeEnvironmentCredential = true,
-        ExcludeWorkloadIdentityCredential = true
-    };
-    DefaultAzureCredential credential = new(credentialOptions);
-#endif
-    string appConfigUrl = builder.Configuration.GetConnectionString("codebreakerconfig") ?? throw new InvalidOperationException("could not read codebreakerconfig");
-    appConfigOptions.Connect(new Uri(appConfigUrl), credential)
-        .Select("bot")
-        .ConfigureKeyVault(keyVaultOptions =>
-        {
-            keyVaultOptions.SetCredential(credential);
-        });
-});
+//builder.Configuration.AddAzureAppConfiguration(appConfigOptions =>
+//{
+//#if DEBUG
+//    DefaultAzureCredential credential = new();
+//#else
+//    string managedIdentityClientId = builder.Configuration["AZURE_CLIENT_ID"] ?? string.Empty;
+//    DefaultAzureCredentialOptions credentialOptions = new()
+//    {
+//        ManagedIdentityClientId = managedIdentityClientId,
+//        ExcludeEnvironmentCredential = true,
+//        ExcludeWorkloadIdentityCredential = true
+//    };
+//    DefaultAzureCredential credential = new(credentialOptions);
+//#endif
+//    string appConfigUrl = builder.Configuration.GetConnectionString("codebreakerconfig") ?? throw new InvalidOperationException("could not read codebreakerconfig");
+//    appConfigOptions.Connect(new Uri(appConfigUrl), credential)
+//        .Select("bot")
+//        .ConfigureKeyVault(keyVaultOptions =>
+//        {
+//            keyVaultOptions.SetCredential(credential);
+//        });
+//});
 
 // Swagger & EndpointDocumentation
 builder.Services.AddEndpointsApiExplorer();
--- ch08/Codebreaker.AppHost/Program.cs
+++ ch08/Codebreaker.AppHost/Program.cs
@@ -4,16 +4,12 @@ string dataStore = builder.Configuration["DataStore"] ?? "InMemory";
 
 builder.AddAzureProvisioning();
 
-//var sqlServer = builder.AddSqlServer("codebreakersql")
-//    .PublishAsAzureSqlDatabase()
-//    .AddDatabase("codebreaker");
-
 var appConfig = builder.AddAzureAppConfiguration("codebreakerconfig");
+    
+// var keyVault = builder.AddAzureKeyVault("codebreakervault");
 
-var keyVault = builder.AddAzureKeyVault("codebreakervault");
-
-builder.AddProject<Projects.Codebreaker_InitalizeAppConfig>("initappconfig")
-    .WithReference(appConfig);
+//builder.AddProject<Projects.Codebreaker_InitalizeAppConfig>("initappconfig")
+//    .WithReference(appConfig);
 
 // the name needs to be reduced until this fix: https://github.com/Azure/azure-dev/issues/3496
 // don't use cb-cosmos, because of deploy failing with the '-' in the name
@@ -21,13 +17,12 @@ var cosmos = builder.AddAzureCosmosDB("cbcosmos")
     .AddDatabase("codebreaker");
 
 var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
-    // .WithReference(sqlServer)
-    .WithReference(cosmos)
-    .WithReference(appConfig)
-    .WithEnvironment("DataStore", dataStore);
+  .WithReference(cosmos)
+  .WithReference(appConfig)
+  .WithEnvironment("DataStore", dataStore);
 
 builder.AddProject<Projects.CodeBreaker_Bot>("bot")
-    .WithReference(appConfig)
+  //  .WithReference(appConfig)
     .WithReference(gameAPIs);
 
 // currently disabled - see https://github.com/PacktPublishing/Pragmatic-Microservices-with-CSharp-and-Azure/issues/81
