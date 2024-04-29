c93fe6c40051c6df0614a812c57307c6926c004a
Christian Nagel <christian@christiannagel.com>
2024-04-29 08:39:53 +0200

    initial blazor wasm auth

---
+++ ch09/BlazorWASMAuth/BlazorWASMAuth/Program.cs
@@ -0,0 +1,33 @@
+using BlazorWASMAuth.Client.Pages;
+using BlazorWASMAuth.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWASMAuth.Client._Imports).Assembly);
+
+app.Run();
