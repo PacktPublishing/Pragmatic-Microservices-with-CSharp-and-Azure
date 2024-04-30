b5f697a73f2d93a59eb0b25b9f007f7348780f2f
Christian Nagel <christian@christiannagel.com>
2024-04-30 16:02:16 +0200

    yarp with transforms

--- ch09/Codebreaker.ApiGateway/Program.cs
+++ ch09/Codebreaker.ApiGateway/Program.cs
@@ -1,20 +1,37 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
+using Yarp.ReverseProxy.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages(); // authentication UI in Views
+
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+//builder.Services.AddAuthorization();
+
+builder.AddServiceDefaults();
 
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddServiceDiscoveryDestinationResolver();
 
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(AzureAd, OpenIdConnectDefaults.AuthenticationScheme)
-    .EnableTokenAcquisitionToCallDownstramApi().
-    .AddDownstreamApi("dfjkfdj", BotApi)
-    .AddInMemoryTokenCache();
+
+// builder.Services.AddRazorPages(); // authentication UI in Views
+
+//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApp(AzureAd, OpenIdConnectDefaults.AuthenticationScheme)
+//    .EnableTokenAcquisitionToCallDownstramApi().
+//    .AddDownstreamApi("dfjkfdj", BotApi)
+//    .AddInMemoryTokenCache();
 
 var app = builder.Build();
 app.MapReverseProxy();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "YARP Proxy");
 
 app.Run();
 
 // https://github.com/microsoft/woodgrove-groceries
+// https://github.com/davidfowl/AspireYarp
+
