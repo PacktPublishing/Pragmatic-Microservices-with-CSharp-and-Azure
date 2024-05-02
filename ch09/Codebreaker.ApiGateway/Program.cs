d75d237f7925991820524abd349e1e2ff359a2a1
Christian Nagel <christian@christiannagel.com>
2024-05-02 17:34:51 +0200

    change to use AuthenticationBuilder

--- ch09/Codebreaker.ApiGateway/Program.cs
+++ ch09/Codebreaker.ApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using Codebreaker.Proxy;
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
@@ -6,9 +8,41 @@ using Yarp.ReverseProxy.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton<TokenService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+    //.EnableTokenAcquisitionToCallDownstreamApi()
+    //.AddDownstreamApi("", "")
+    //.AddInMemoryTokenCache();
+
 //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
-//builder.Services.AddAuthorization();
+//    .AddMicrosoftIdentityWebApi(bearerOptions =>
+//    {
+//        builder.Configuration.Bind("AzureAdB2C", bearerOptions);
+//        //        bearerOptions.TokenValidationParameters.NameClaimType = "name";
+//    }, identityOptions =>
+//    {
+//        builder.Configuration.Bind("AzureAdB2C", identityOptions);
+//    });
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("playPolicy", config =>
+    {
+        config.RequireScope("Games.Play");
+    })
+   .AddPolicy("queryPolicy", config =>
+   {
+        config.RequireScope("Games.Query");
+        config.RequireAuthenticatedUser();
+   })
+   .AddPolicy("botPolicy", config =>
+   {
+       config.RequireScope("Bot.Play");
+    //   config.RequireClaim("role", [ "Bot" ]);
+       config.RequireAuthenticatedUser();
+   });
 
 builder.AddServiceDefaults();
 
@@ -19,13 +53,11 @@ builder.Services.AddReverseProxy()
 
 // builder.Services.AddRazorPages(); // authentication UI in Views
 
-//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApp(AzureAd, OpenIdConnectDefaults.AuthenticationScheme)
-//    .EnableTokenAcquisitionToCallDownstramApi().
-//    .AddDownstreamApi("dfjkfdj", BotApi)
-//    .AddInMemoryTokenCache();
-
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapReverseProxy();
 
 app.MapGet("/", () => "YARP Proxy");
