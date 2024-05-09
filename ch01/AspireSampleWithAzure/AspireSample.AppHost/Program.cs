df8988ed7357f54b07d7407efadfac74c7c18e67
Christian Nagel <christian@christiannagel.com>
2024-05-09 22:05:46 +0200

    add azure to aspiresample

--- ch01/AspireSampleWithAzure/AspireSample.AppHost/Program.cs
+++ ch01/AspireSampleWithAzure/AspireSample.AppHost/Program.cs
@@ -1,7 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var keyVault = builder.AddAzureKeyVault("secrets");
+
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice")
-    .WithReplicas(3);
+    .WithReplicas(3)
+    .WithReference(keyVault);
 
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
     .WithExternalHttpEndpoints()
