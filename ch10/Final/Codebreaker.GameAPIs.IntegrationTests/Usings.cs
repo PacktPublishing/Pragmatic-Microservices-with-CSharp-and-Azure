d300787739c0e78f7b3c44287a980d40a8ee7319
Christian Nagel <christian@christiannagel.com>
2024-05-14 16:53:06 +0200

    remove integration tests without aspire

--- ch10/Final/Codebreaker.GameAPIs.IntegrationTests/Usings.cs
+++
@@ -1,9 +0,0 @@
-global using System.Net;
-global using System.Net.Http.Json;
-
-global using Codebreaker.GameAPIs.Models;
-
-global using Microsoft.AspNetCore.Mvc.Testing;
-global using Microsoft.Extensions.Hosting;
-
-global using Xunit;
