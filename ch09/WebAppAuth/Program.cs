1d4e8f51263a93f77f90b3f0767fd9a9558ef5c3
Christian Nagel <christian@christiannagel.com>
2024-05-14 11:45:11 +0200

    web app auth

--- ch09/WebAppAuth/Program.cs
+++ ch09/WebAppAuth/Program.cs
@@ -1,18 +1,25 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+IConfigurationSection scopeSection = builder.Configuration.GetSection("AzureAdB2C").GetSection("Scopes");
+string[] scopes = scopeSection.Get<string[]>() ?? [];
+
 // Add services to the container.
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"))
+    .EnableTokenAcquisitionToCallDownstreamApi(scopes)
+    .AddInMemoryTokenCaches();
+
 builder.Services.AddRazorPages()
     .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
