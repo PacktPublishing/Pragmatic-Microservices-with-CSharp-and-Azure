1ff145c36d7b3dda7fbc1b4ebc75fae4467b4e0b
Christian Nagel <christian@christiannagel.com>
2024-05-15 15:29:33 +0200

    ch13 preview 7

---
+++ ch13/Codebreaker.AppHost/ApplicationBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+using System.Reflection;
+
+namespace Codebreaker.AppHost;
+
+internal static class ApplicationBuilderExtensions
+{
+    public static void AddUserSecretsForPrometheusEnvironment(this IDistributedApplicationBuilder applicationBuilder)
+    {
+        if (Environment.GetEnvironmentVariable("STARTUP_MODE") == "OnPremises")
+        {
+            string appName = applicationBuilder.Environment.ApplicationName;
+            if (!string.IsNullOrEmpty(appName))
+            {
+                var appAssembly = Assembly.Load(new AssemblyName(appName));
+                applicationBuilder.Configuration.AddUserSecrets(appAssembly, optional: true);
+            }
+        }
+    }
+}
--- ch13/Codebreaker.AppHost/Program.cs
+++ ch13/Codebreaker.AppHost/Program.cs
@@ -1,38 +1,76 @@
 
+using Codebreaker.AppHost;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 string dataStore = builder.Configuration["DataStore"] ?? "InMemory";
-
-builder.AddAzureProvisioning();
-
-var logs = builder.AddAzureLogAnalyticsWorkspace("logs");
-var insights = builder.AddAzureApplicationInsights("insights", logs);
-var signalR = builder.AddAzureSignalR("signalr");
+string startupMode = builder.Configuration["STARTUP_MODE"] ?? "Azure";  // specified with environment variables in the launch profile
 
 var redis = builder.AddRedis("redis")
     .WithRedisCommander()
     .PublishAsContainer();
 
-var cosmos = builder.AddAzureCosmosDB("codebreakercosmos")
-    .AddDatabase("codebreaker");
+if (startupMode == "OnPremises")
+{
+    var sqlServer = builder.AddSqlServer("sql")
+        .WithDataVolume()
+        .PublishAsContainer()
+        .AddDatabase("CodebreakerSql");
 
-var live = builder.AddProject<Projects.Codebreaker_Live>("live")
-    .WithExternalHttpEndpoints()
-    .WithReference(insights)
-    .WithReference(signalR);
+    var grafana = builder.AddContainer("grafana", "grafana/grafana")
+        .WithBindMount("../grafana/config", "/etc/grafana", isReadOnly: true)
+        .WithBindMount("../grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
+        .WithHttpEndpoint(targetPort: 3000, name: "grafana-http");
 
-var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
-    .WithExternalHttpEndpoints()
-    .WithReference(cosmos)
-    .WithReference(redis)
-    .WithReference(insights)
-    .WithReference(live)
-    .WithEnvironment("DataStore", dataStore);
+    builder.AddUserSecretsForPrometheusEnvironment();
 
-builder.AddProject<Projects.CodeBreaker_Bot>("bot")
-    .WithExternalHttpEndpoints()
-    .WithReference(insights)
-    .WithReference(gameAPIs);
+    var prometheus = builder.AddContainer("prometheus", "prom/prometheus")
+       .WithBindMount("../prometheus", "/etc/prometheus", isReadOnly: true)
+       .WithHttpEndpoint(/* This port is fixed as it's referenced from the Grafana config */ port: 9090, targetPort: 9090);
+
+    var live = builder.AddProject<Projects.Codebreaker_Live>("live")
+        .WithExternalHttpEndpoints();
+
+    var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
+        .WithReference(sqlServer)
+        .WithReference(redis)
+        .WithEnvironment("DataStore", dataStore)
+        .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("grafana-http"))
+        .WithEnvironment("StartupMode", startupMode);
+
+    builder.AddProject<Projects.CodeBreaker_Bot>("bot")
+        .WithReference(gameAPIs)
+        .WithEnvironment("StartupMode", startupMode);
+}
+else
+{
+    var logs = builder.AddAzureLogAnalyticsWorkspace("logs");
+    var appInsights = builder.AddAzureApplicationInsights("insights", logs);
+    var signalR = builder.AddAzureSignalR("signalr");
+
+    var cosmos = builder.AddAzureCosmosDB("codebreakercosmos")
+        .AddDatabase("codebreaker");
+
+    var live = builder.AddProject<Projects.Codebreaker_Live>("live")
+        .WithExternalHttpEndpoints()
+        .WithReference(appInsights)
+        .WithReference(signalR);
+
+    var gameAPIs = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
+        .WithExternalHttpEndpoints()
+        .WithReference(cosmos)
+        .WithReference(redis)
+        .WithReference(appInsights)
+        .WithReference(live)
+        .WithEnvironment("DataStore", dataStore)
+        .WithEnvironment("StartupMode", startupMode)
+        .WithReplicas(2);
+
+    builder.AddProject<Projects.CodeBreaker_Bot>("bot")
+        .WithExternalHttpEndpoints()
+        .WithReference(gameAPIs)
+        .WithReference(appInsights)
+        .WithEnvironment("StartupMode", startupMode);
+}
 
 builder.Build().Run();
