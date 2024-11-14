a04086c5e1c2b0627e3369bf18d7c58ac85371b7
Christian Nagel <christian@christiannagel.com>
2024-11-14 12:30:21 +0100

    ch03 dotnet 9 with CPM

--- ch03/Codebreaker.AppHost/Program.cs
+++ ch03/Codebreaker.AppHost/Program.cs
@@ -1,8 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// change the DataStore setting in Properties/launchSettings.json to either Cosmos or SqlServer
 string dataStore = builder.Configuration["DataStore"] ?? "InMemory";
 
-// change the DataStore setting in appsettings.json
 var gameApis = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
     .WithExternalHttpEndpoints()
     .WithEnvironment("DataStore", dataStore);
@@ -10,24 +10,26 @@ var gameApis = builder.AddProject<Projects.Codebreaker_GameAPIs>("gameapis")
 if (dataStore == "SqlServer")
 {
     var sqlServer = builder.AddSqlServer("sql")
-        .WithDataVolume()
+        .WithDataVolume();
+    var sqlDB = sqlServer
         .AddDatabase("CodebreakerSql", "codebreaker");
 
     gameApis
-        .WithReference(sqlServer);
+        .WithReference(sqlDB)
+        .WaitFor(sqlServer);
 }
 else if (dataStore == "Cosmos")
 {
     // When using Cosmos with the following connection string, you need to start the Azure Cosmos emulator on your system, and create a Codebreaker database in this emulator (see information in the readme file)
-    var cosmos = builder.AddConnectionString("codebreakercosmos");
+    var cosmosDB = builder.AddConnectionString("codebreakercosmos");
 
-    // Comment the previous line and uncomment the next three lines to use the Azure Cosmos emulator in a Docker container (if this is running and doesn't have certificate / timing / HTTPS issues (see the readme file)
-    //var cosmos = builder.AddAzureCosmosDB("codebreakercosmos")
-    //    .AddDatabase("codebreaker")
-    //    .RunAsEmulator();
+    //// Comment the previous line and uncomment the next three lines to use the Azure Cosmos emulator in a Docker container (if this is running and doesn't have certificate / timing / HTTPS issues (see the readme file)
+    //var cosmos = builder.AddAzureCosmosDB("codebreakercosmos");
+    //var cosmosDB = cosmos
+    //    .AddDatabase("codebreaker");
 
     gameApis
-        .WithReference(cosmos);
+        .WithReference(cosmosDB);
 }
 
 builder.Build().Run();
