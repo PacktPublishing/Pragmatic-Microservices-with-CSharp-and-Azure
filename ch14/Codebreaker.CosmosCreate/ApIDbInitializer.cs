8946889fcbf9f9a15cf658e20f3496c17dcca6a0
Christian Nagel <christian@christiannagel.com>
2024-12-07 13:46:15 +0100

    ch14 .net 9 updates

--- ch14/Codebreaker.CosmosCreate/ApIDbInitializer.cs
+++ ch14/Codebreaker.CosmosCreate/ApIDbInitializer.cs
@@ -1,10 +1,11 @@
 ﻿using Codebreaker.Data.Cosmos;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry.Trace;
+
 using System.Diagnostics;
 
 namespace Codebreaker.SqlServerMigration;
@@ -29,7 +30,7 @@ internal class ApiDbInitializer(
         }
         catch (Exception ex)
         {
-            activity?.RecordException(ex);
+            activity?.AddException(ex);
             throw;
         }
 
